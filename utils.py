653c56975eaa78f158dd592c69170675add8037e
sangmun <sml8648@naver.com>
2023-05-15 20:36:42 +0900

    ci/cd test

--- utils.py
+++ utils.py
@@ -3,20 +3,22 @@ import httpx
 import asyncio
 from fastapi import HTTPException
 import json
-######
+
+
 class InputStr(BaseModel):
-    #name:str
-    #shape:[]
-    #datatype:str
-    #parameters":[]
-    #data:str
-    text:str
-    target:tuple
+    # name:str
+    # shape:[]
+    # datatype:str
+    # parameters":[]
+    # data:str
+    text: str
+
 
 # async get request
 async def request(client, url):
     response = await client.get(url)
     return response
+
 
 async def request_handler(url):
     async with httpx.AsyncClient() as client:
@@ -24,10 +26,12 @@ async def request_handler(url):
         result = await asyncio.gather(tasks)
         return result[0]
 
+
 # async post request
 async def request_post(client, url, data):
     response = await client.post(url, data=data)
     return response
+
 
 async def request_post_handler(url, file_path):
     async with httpx.AsyncClient() as client:
@@ -37,10 +41,10 @@ async def request_post_handler(url, file_path):
         tasks = request_post(client, url, data=file_content)
         result = await asyncio.gather(tasks)
         return result[0]
-    
-def error_handler(response):
 
-    error_message = json.loads(response.text)['message']
+
+def error_handler(response):
+    error_message = json.loads(response.text)["message"]
 
     if response.status_code == 404:
         raise HTTPException(status_code=404, detail=error_message)
